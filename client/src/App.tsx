a67cf6d9275ed145f64c8e1c8b0891f3f03a5d1e
VaaniBhatnagar <12253595-VaaniBhatnagar@users.noreply.replit.com>
2026-01-21 00:41:37 +0000

    Restored to '911621256f5e64ef1a76894a53b8980e17c43a37'
    Replit-Restored-To: 911621256f5e64ef1a76894a53b8980e17c43a37

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,4 @@
-import { Switch, Route, Router as WouterRouter } from "wouter";
-import { useHashLocation } from "wouter/use-hash-location";
+import { Switch, Route } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
@@ -11,13 +10,11 @@ import ProjectDetail from "./pages/project-detail";
 
 function Router() {
   return (
-    <WouterRouter hook={useHashLocation}>
-      <Switch>
-        <Route path="/" component={Home} />
-        <Route path="/project/:id" component={ProjectDetail} />
-        <Route component={NotFound} />
-      </Switch>
-    </WouterRouter>
+    <Switch>
+      <Route path="/" component={Home} />
+      <Route path="/project/:id" component={ProjectDetail} />
+      <Route component={NotFound} />
+    </Switch>
   );
 }
 
